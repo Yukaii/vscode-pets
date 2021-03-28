f80113457ec4437d363480ebd808d4a335ce2f3a
Yukai Huang <yukaihuangtw@gmail.com>
2021-03-28 23:35:10 +0800

    Webview view poc

--- src/extension/extension.ts
+++ src/extension/extension.ts
@@ -84,6 +84,12 @@ export function activate(context: vscode.ExtensionContext) {
 		})
 	);
 
+	const spec = PetSpecification.fromConfiguration();
+	const provider = new PetsViewProvider(context.extensionUri, context.extensionPath, spec.color, spec.type, spec.size);
+	
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider(PetsViewProvider.viewType, provider));
+
 	context.subscriptions.push(
 		vscode.commands.registerCommand('vscode-pets.throw-ball', () => {
 			if (PetPanel.currentPanel) {
@@ -188,6 +194,97 @@ function normalizeColor(petColor: PetColor, petType: PetType): PetColor {
 		petColor === PetColor.green)
 		{return PetColor.brown;}
 	return petColor;
+}
+
+
+class PetsViewProvider implements vscode.WebviewViewProvider {
+	public static readonly viewType = 'vscode-pets.petsView';
+
+	private _view?: vscode.WebviewView;
+	private _petMediaPath: string;
+	private _disposables: vscode.Disposable[] | undefined;
+	
+	constructor(
+		private readonly _extensionUri: vscode.Uri, 
+		extensionPath: string, 
+		petColor: PetColor, 
+		petType:PetType, 
+		petSize:PetSize	
+	) {
+		this._petMediaPath = path.join(extensionPath, 'media');
+	}
+
+	resolveWebviewView(webviewView: vscode.WebviewView, context: vscode.WebviewViewResolveContext<unknown>, token: vscode.CancellationToken): void | Thenable<void> {
+		this._view = webviewView;
+
+		webviewView.webview.options = {
+			// Allow scripts in the webview
+			enableScripts: true,
+
+			localResourceRoots: [
+				this._extensionUri
+			]
+		};
+		
+		webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+		
+		webviewView.webview.onDidReceiveMessage(
+			message => {
+				switch (message.command) {
+					case 'alert':
+						vscode.window.showErrorMessage(message.text);
+						return;
+				}
+			},
+			null,
+			this._disposables
+		);		
+	}
+	
+
+	private _getHtmlForWebview(webview: vscode.Webview) {
+		// Local path to main script run in the webview
+		const scriptPathOnDisk = vscode.Uri.joinPath(this._extensionUri, 'media', 'main-bundle.js');
+
+		// And the uri we use to load this script in the webview
+		const scriptUri = webview.asWebviewUri(scriptPathOnDisk);
+
+		// Local path to css styles
+		const styleResetPath = vscode.Uri.joinPath(this._extensionUri, 'media', 'reset.css');
+		const stylesPathMainPath = vscode.Uri.joinPath(this._extensionUri, 'media', 'pets.css');
+
+		// Uri to load styles into webview
+		const stylesResetUri = webview.asWebviewUri(styleResetPath);
+		const stylesMainUri = webview.asWebviewUri(stylesPathMainPath);
+
+		// Get path to resource on disk
+		const basePetUri = webview.asWebviewUri(vscode.Uri.file(path.join(this._petMediaPath)));
+
+		// Use a nonce to only allow specific scripts to be run
+		const nonce = getNonce();
+
+		return `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<!--
+					Use a content security policy to only allow loading images from https or from our extension directory,
+					and only allow scripts that have a specific nonce.
+				-->
+				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; img-src ${webview.cspSource} https:; script-src 'nonce-${nonce}';">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<link href="${stylesResetUri}" rel="stylesheet">
+				<link href="${stylesMainUri}" rel="stylesheet">
+				<title>VS Code Pets</title>
+			</head>
+			<body>
+				<canvas id="petCanvas"></canvas><div id="petsContainer"></div>
+				<script nonce="${nonce}" src="${scriptUri}"></script>
+				<script nonce="${nonce}">petApp.petPanelApp("${basePetUri}", "${PetColor.black}", "${PetSize.nano}", "${PetType.cat}");</script>
+				
+			</body>
+			</html>`;
+	}
 }
 
 /**
